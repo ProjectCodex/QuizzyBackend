24e8e91255b58991d4367fef1e34a45e437019b3
German Gamboa <germangamboa95@gmail.com>
2019-04-07 23:52:04 -0400

    adding express and testing branch rule this should fail

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-function sum(a, b) {
-  return a + b;
-}
-module.exports = sum;
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    status: "I AM ALIVE"
+  });
+});
+
+const _PORT = process.env._PORT || 3000;
+
+app.listen(_PORT, err => {
+  if (err) throw new Error("Something went very wrong!");
+  console.log(`Server is running on port: ${_PORT}`);
+});
